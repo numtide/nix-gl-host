8aac62b275264a89a43fcd0a2c37043eff8f5de3
Félix Baylac Jacqué <felix@alternativebit.fr>
2023-04-04 18:23:26 +0200

    RIIR: define core datatypes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{time::SystemTime, path::{PathBuf}};
+
 use clap::Parser;
 
 #[derive(Parser, Debug)]
@@ -16,6 +18,43 @@ struct CLI {
 
     ///The args passed to the wrapped binary.
     args: Vec<String>
+}
+
+/// Represents a GPU host library.
+///
+/// We uniquely identify a library through its fullpath, the last
+/// modification date and its length.
+///
+/// We used to content-adress those, but hasing all these files turned
+/// out slowing down the wrapper quite significantly.
+struct ResolvedLib {
+    name: String,
+    fullpath: PathBuf,
+    last_modification: SystemTime,
+    len: u64
+}
+
+/// Represents a host library path entry. This entry can contain many
+/// GPU dynamic libraries.
+struct HostLibraryPath {
+    fullpath: PathBuf,
+    /// GLX-related libraries contained in this library path entry.
+    glx: Vec<ResolvedLib>,
+    /// EGL-related libraries contained in this library path entry.
+    egl: Vec<ResolvedLib>,
+    /// Cuda-related libraries contained in this library path entry.
+    cuda: Vec<ResolvedLib>,
+    /// Generic/helper libraries contained in this library path entry.
+    generic: Vec<ResolvedLib>,
+}
+
+/// Encapsulates all the dynamically shared objects living in the
+/// nix-gl-host cache.
+///
+/// We mostly use it to serialize what's in the cache on the disk and
+/// compare this content to what we scanned in the host system.
+struct CacheDirContent {
+    paths: Vec<HostLibraryPath>
 }
 
 fn main() {
